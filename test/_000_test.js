99081f9bc3f50ea557711f0bdd327368e59b8a0c
PSK <wiv821253@gmail.com>
2020-04-19 21:07:09 +0900

    add chai

--- test/_000_test.js
+++ test/_000_test.js
@@ -1,9 +1,17 @@
 const Province = require("./app.test");
 const assert = require("assert")
+const chai = require("chai")
 
 describe('tests', () => {
     it('shortfall', () => {
         const asia = new Province({name:"hello"})
-        assert.equal(asia._name, "a");
+        assert.equal(asia._name, "hello");
+    })
+})
+
+describe('province', () => {
+    it('shortfall', () => {
+        const asia = new Province({name:"hello"})
+        chai.expect(asia.name).equal("hello")
     })
 })
--- test/app.test.js
+++ test/app.test.js
@@ -2,5 +2,6 @@ const Province = class Province {
     constructor(doc) {
         this._name = doc.name;
     }
+    get name() {return this._name}
 }
 module.exports = Province
