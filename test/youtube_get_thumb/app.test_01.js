2990d16ef46a1cb1f807402e00fae5de9100573d
PSK <wiv821253@gmail.com>
2020-05-13 11:01:22 +0900

    uri test

--- test/youtube_get_thumb/app.test_01.js
+++ test/youtube_get_thumb/app.test_01.js
@@ -6,22 +6,29 @@ const before = mocha.before;
 
 const extractKey = require("./extractKey");
 
-describe('# 유튜브 id 추출', () => {
+describe('# 유튜브 uri 조합', () => {
     let thumbBase = "https://img.youtube.com/vi/{{key}}/hqdefault.jpg"
 
-    let youtubeKey
-    let expected = `https://img.youtube.com/vi/k-senVR0q8M/hqdefault.jpg`
     let uri
+    let expected = `https://img.youtube.com/vi/k-senVR0q8M/hqdefault.jpg`
+    let actual
+
     let expectedKey
+    let actualKey
+
     before(() => {
         uri = `https://www.youtube.com/embed/k-senVR0q8M?list=PL7FKPrjfmaNx4O5PzFuXm3RTAAgEyKjds?wmode=transparent`;
-        youtubeKey = `k-senVR0q8M`
     })
 
     it('parameter 있을 때', () => {
-        expectedKey = uri.replace("{{key}}", extractKey(uri));
-        assert.strictEqual(youtubeKey, expectedKey, "actual key equal extract key")
+        expectedKey = `k-senVR0q8M`
+        actualKey = extractKey(uri);
+        assert.strictEqual(actualKey, expectedKey, "actual key equal extract key")
     })
 
+    it('uri 전체 조합 가져오기', () => {
+        actual = thumbBase.replace("{{key}}", extractKey(uri));
+        assert.strictEqual(actual, expected, "uri equal")
+    })
 })
 
