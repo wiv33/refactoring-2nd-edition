ec67220e4fc184241197d13765a623db6e064f8a
ps <wiv821253@gmail.com>
2020-05-15 21:34:24 +0900

    add all fail test code

--- tdd/src/main/java/org/psawesome/tdd/myArray/MyArrayList.java
+++ tdd/src/main/java/org/psawesome/tdd/myArray/MyArrayList.java
@@ -1,10 +1,147 @@
 package org.psawesome.tdd.myArray;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * package: org.psawesome.tdd.myArray
  * author: PS
  * DATE: 2020-05-14 목요일 20:52
  */
-public class MyArrayList {
+public class MyArrayList<T> implements List<T> {
 
+    int size;
+    private T[] array;
+
+    @SuppressWarnings("unchecked")
+    public MyArrayList() {
+        array = (T[]) new Object[10];
+        size = 0;
+
+    }
+
+    @Override
+    public int size() {
+        return this.array.length;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T element) {
+        if (size >= this.array.length) {
+            @SuppressWarnings("unchecked")
+            T[] sizeUp = (T[]) new Object[this.array.length * 2];
+            System.arraycopy(this.array, 0, sizeUp, 0, sizeUp.length);
+            this.array = sizeUp;
+        }
+        this.array[size] = element;
+        size++;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
 }
--- tdd/src/test/java/org/psawesome/tdd/myArray/MyArrayListTest.java
+++ tdd/src/test/java/org/psawesome/tdd/myArray/MyArrayListTest.java
@@ -1,9 +1,14 @@
 package org.psawesome.tdd.myArray;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * package: org.psawesome.tdd.myArray
@@ -11,10 +16,279 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * DATE: 2020-05-14 목요일 20:57
  */
 class MyArrayListTest {
+    protected List<Integer> mylist;
+    protected List<Integer> list;
+
+    @BeforeEach
+    void setUp() {
+        list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        mylist = new MyArrayList<Integer>();
+        mylist.addAll(list);
+    }
 
     @Test
     void init() {
-        MyArrayList list = new MyArrayList();
+        list = new MyArrayList<>();
         assertNotNull(list);
     }
+
+    @Test
+    public void testMyList() {
+        Assertions.assertEquals(mylist.size(), 3);
+    }
+
+    /**
+     * Test method for {@link MyArrayList#add(Object)}.
+     */
+    @Test
+    public void testAddT() {
+        for (int i = 4; i < 20; i++) {
+            mylist.add(i);
+        }
+        //System.out.println(Arrays.toString(mal.toArray()));
+        assertEquals(mylist.get(18), Integer.valueOf(19));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#add(int, Object)}.
+     */
+    @Test
+    public void testAddIntT() {
+        mylist.add(1, 5);
+        //System.out.println(Arrays.toString(mal.toArray()));
+        assertEquals(mylist.get(1), Integer.valueOf(5));
+        assertEquals(mylist.size(), 4);
+
+        try {
+            mylist.set(-1, 0);
+            fail();
+        } catch (IndexOutOfBoundsException e) {} // good
+
+        try {
+            mylist.set(4, 0);
+            fail();
+        } catch (IndexOutOfBoundsException e) {} // good
+
+        mylist.add(0, 6);
+        //System.out.println(Arrays.toString(mal.toArray()));
+        assertEquals(mylist.get(0), 6);
+
+        mylist.add(5, 7);
+        //System.out.println(Arrays.toString(mal.toArray()));
+        assertEquals(mylist.get(5), Integer.valueOf(7));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#addAll(java.util.Collection)}.
+     */
+    @Test
+    public void testAddAllCollectionOfQextendsT() {
+        mylist.addAll(list);
+        mylist.addAll(list);
+        mylist.addAll(list);
+        assertEquals(mylist.size(), 12);
+        assertEquals(mylist.get(5), Integer.valueOf(3));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#clear()}.
+     */
+    @Test
+    public void testClear() {
+        mylist.clear();
+        assertEquals(mylist.size(), 0);
+    }
+
+    /**
+     * Test method for {@link MyArrayList#contains(Object)}.
+     */
+    @Test
+    public void testContains() {
+        assertEquals(mylist.contains(1), (true));
+        assertEquals(mylist.contains(4), (false));
+        assertEquals(mylist.contains(null), (false));
+        mylist.add(null);
+        assertEquals(mylist.contains(null), (true));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#containsAll(java.util.Collection)}.
+     */
+    @Test
+    public void testContainsAll() {
+        assertEquals(mylist.containsAll(list), (true));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#get(int)}.
+     */
+    @Test
+    public void testGet() {
+        assertEquals(mylist.get(1), (Integer.valueOf(2)));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#indexOf(Object)}.
+     */
+    @Test
+    public void testIndexOf() {
+        assertEquals(mylist.indexOf(1), (0));
+        assertEquals(mylist.indexOf(2), (1));
+        assertEquals(mylist.indexOf(3), (2));
+        assertEquals(mylist.indexOf(4), (-1));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#indexOf(Object)}.
+     */
+    @Test
+    public void testIndexOfNull() {
+        assertEquals(mylist.indexOf(null), (-1));
+        mylist.add(null);
+        assertEquals(mylist.indexOf(null), (3));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#isEmpty()}.
+     */
+    @Test
+    public void testIsEmpty() {
+        assertEquals(mylist.isEmpty(), (false));
+        mylist.clear();
+        assertEquals(mylist.isEmpty(), (true));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#iterator()}.
+     */
+    @Test
+    public void testIterator() {
+        Iterator<Integer> iter = mylist.iterator();
+        assertEquals(iter.next(), (Integer.valueOf(1)));
+        assertEquals(iter.next(), (Integer.valueOf(2)));
+        assertEquals(iter.next(), (Integer.valueOf(3)));
+        assertEquals(iter.hasNext(), (false));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#lastIndexOf(Object)}.
+     */
+    @Test
+    public void testLastIndexOf() {
+        mylist.add(2);
+        assertEquals(mylist.lastIndexOf(2), (3));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#remove(Object)}.
+     */
+    @Test
+    public void testRemoveObject() {
+        boolean flag = mylist.remove(Integer.valueOf(2));
+        assertEquals(flag, (true));
+        assertEquals(mylist.size(), (2));
+        assertEquals(mylist.get(1), (Integer.valueOf(3)));
+        //System.out.println(Arrays.toString(mal.toArray()));
+
+        flag = mylist.remove(Integer.valueOf(1));
+        assertEquals(flag, (true));
+        assertEquals(mylist.size(), (1));
+        assertEquals(mylist.get(0), (Integer.valueOf(3)));
+        //System.out.println(Arrays.toString(mal.toArray()));
+
+        flag = mylist.remove(Integer.valueOf(5));
+        assertEquals(flag, (false));
+        assertEquals(mylist.size(), (1));
+        assertEquals(mylist.get(0), (Integer.valueOf(3)));
+        //System.out.println(Arrays.toString(mal.toArray()));
+
+        flag = mylist.remove(Integer.valueOf(3));
+        assertEquals(flag, (true));
+        assertEquals(mylist.size(), (0));
+        //System.out.println(Arrays.toString(mal.toArray()));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#remove(int)}.
+     */
+    @Test
+    public void testRemoveInt() {
+        Integer val = mylist.remove(1);
+        assertEquals(val, (Integer.valueOf(2)));
+        assertEquals(mylist.size(), (2));
+        assertEquals(mylist.get(1), (Integer.valueOf(3)));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#removeAll(java.util.Collection)}.
+     */
+    @Test
+    public void testRemoveAll() {
+        mylist.removeAll(list);
+        assertEquals(mylist.size(), (0));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#set(int, Object)}.
+     */
+    @Test
+    public void testSet() {
+        Integer val = mylist.set(1, 5);
+        assertEquals(val, (Integer.valueOf(2)));
+
+        val = mylist.set(0, 6);
+        assertEquals(val, (Integer.valueOf(1)));
+
+        val = mylist.set(2, 7);
+        assertEquals(val, (Integer.valueOf(3)));
+
+        // return value should be 2
+        // list should be [6, 5, 7]
+        assertEquals(mylist.get(0), (Integer.valueOf(6)));
+        assertEquals(mylist.get(1), (Integer.valueOf(5)));
+        assertEquals(mylist.get(2), (Integer.valueOf(7)));
+        //System.out.println(Arrays.toString(mal.toArray()));
+
+        try {
+            mylist.set(-1, 0);
+            fail();
+        } catch (IndexOutOfBoundsException e) {} // good
+
+        try {
+            mylist.set(4, 0);
+            fail();
+        } catch (IndexOutOfBoundsException e) {} // good
+    }
+
+    /**
+     * Test method for {@link MyArrayList#size()}.
+     */
+    @Test
+    public void testSize() {
+        assertEquals(mylist.size(), (3));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#subList(int, int)}.
+     */
+    @Test
+    public void testSubList() {
+        mylist.addAll(list);
+        List<Integer> sub = mylist.subList(1, 4);
+        assertEquals(sub.get(1), (Integer.valueOf(3)));
+    }
+
+    /**
+     * Test method for {@link MyArrayList#toArray()}.
+     */
+    @Test
+    public void testToArray() {
+        Object[] array = mylist.toArray();
+        assertEquals((Integer)array[0], (Integer.valueOf(1)));
+    }
+
 }
