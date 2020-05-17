b9ee06b1b2edf4e99aee92b4fcba248967af2859
PSK <wiv821253@gmail.com>
2020-05-17 11:40:57 +0900

    add JSoup

---
+++ tdd/src/main/java/org/psawesome/tdd/treeCircuit/WikiNodeExam.java
@@ -0,0 +1,9 @@
+package org.psawesome.tdd.treeCircuit;
+
+/**
+ * package: org.psawesome.tdd.treeCircuit
+ * author: PS
+ * DATE: 2020-05-17 일요일 11:38
+ */
+public class WikiNodeExam {
+}
---
+++ tdd/src/test/java/org/psawesome/tdd/treeCircuit/WikiNodeExamTest.java
@@ -0,0 +1,23 @@
+package org.psawesome.tdd.treeCircuit;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * package: org.psawesome.tdd.treeCircuit
+ * author: PS
+ * DATE: 2020-05-17 일요일 11:38
+ */
+class WikiNodeExamTest {
+
+
+    @Test
+    void testSelectJSoup() {
+        Connection connect = Jsoup.connect("");
+        Assertions.assertNotNull(connect);
+    }
+}
