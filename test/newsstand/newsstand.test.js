61209248770b9a358ece42acb6427e15b98daa10
PSK <wiv821253@gmail.com>
2020-05-20 14:51:27 +0900

    add expect

--- test/newsstand/newsstand.test.js
+++ test/newsstand/newsstand.test.js
@@ -23,15 +23,22 @@ describe(`JSON 내보내기`, () => {
     }
 
     mocha.before(() => {
-        let arr = []
-        let ids = ['202020', '52352', '6344634', '734341', '467457', '26236523']
-        ids.forEach((v) => arr.push({nsid: v}))
-
-        expected = {
-            editorId: `필성<${email}>`,
-            template: 'top-single',
-            headlineArticles: arr,
-            feedbackEmail: `${email};${email}`
+        const urls = ['aaa','bbb','ccc','ddd','eee','fff','ggg','hhh','iii','jjj','kkk']
+        const subTitleListStr = ['qwer', 'asdf', 'zxcv', 'hgfd', 'trew', 'jtr', 'mry', 'rew']
+        param = {
+            "siteSeq": "siteSeq",
+            "siteCd": "siteCd",
+            "articleSeq": "articleSeq",
+            "chkServer": "chkServer",
+            "deployReason": "deployReason",
+            "deployFileNm": "deployFileNm",
+            "deployFileDir": "deployFileDir",
+            "uniqueId": "uniqueId",
+            "title": "title",
+            "summary": "summary",
+            "audioYn": 'Y',
+            "targetLink": urls,
+            "subTitleList": subTitleListStr
         }
     })
 
@@ -50,13 +57,29 @@ describe(`JSON 내보내기`, () => {
     it(`거기서 원하는 json 생성`, function (done) {
         this.timeout(2000); // default
 
+        let arr = []
+        let ids = ['202020', '52352', '6344634', '734341', '467457', '26236523']
+        ids.forEach((v) => arr.push({nsid: v}))
+
+        expected = {
+            editorId: `필성<${email}>`,
+            template: 'top-single',
+            headlineArticles: arr,
+            feedbackEmail: `${email};${email}`
+        }
+
         axios.post("/article/newsstand/json.do", param, {
             headers: {
                 "Cookie": `JSESSIONID=${sessionId}`
-            }
+            },
+            withCredentials: true
         })
         .then(res => {
             console.log(res);
+            assert.strictEqual(expected.editorId, res.editorId)
+            assert.strictEqual(expected.template, res.template)
+            // assert.strictEqual(expected.headlineArticles, res.headlineArticles)
+            assert.strictEqual(expected.feedbackEmail, res.feedbackEmail)
             done();
         })
 
