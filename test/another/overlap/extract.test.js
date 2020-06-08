7b3c92e18fe86e73c5fdeeffb373364cca132091
PSK <wiv821253@gmail.com>
2020-06-08 13:49:30 +0900

    check success

--- test/another/overlap/extract.test.js
+++ test/another/overlap/extract.test.js
@@ -6,21 +6,35 @@
 const assert = require("assert");
 const expectData = require("./result");
 const appendFunc = require("./existing_function");
-
+const axios = require('axios');
 
 describe('함수 추출하기', function () {
   let actual
 
   describe('should be equal data', function () {
-    it('should be result match', function () {
+    it('should be result match', function (done) {
+      const url = "http://localhost:8060/article/management.do?startCount=0&startDate=2020.05.08&endDate=2020.05.08&keyword=&sort=asc";
+      axios.get(url, {
+        headers: {
+          Cookie: `JSESSIONID=669A916E9469C8C074E4845A54206A03`
+        },
+        responseType: "json",
+      })
+      .then(res => {
         const expect = appendFunc(expectData);
-        actual =
+        actual = appendFunc(res.data);
         assert.deepStrictEqual(actual, expect);
+
+
+        done();
+
+      })
+
     });
 
-      it('should be', function () {
-          assert.strictEqual()
-      });
+    it('should be', function () {
+      assert.strictEqual()
+    });
   });
 
 });
