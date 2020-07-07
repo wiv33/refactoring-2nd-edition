be8b6387c00dffeb6a4eb07bdf36e93aacb4eb5d
PSK <wiv821253@gmail.com>
2020-07-07 17:54:32 +0900

    객체 비교 테스트

--- test/_003_obj_class_diff.test.js
+++ test/_003_obj_class_diff.test.js
@@ -2,6 +2,14 @@ const common = require("./default.test");
 const assert = common.assert;
 const mocha = common.mocha;
 const beforeEach = mocha.beforeEach;
+const afterEach = mocha.afterEach;
+
+function Generate() {
+  return new Object({id: "ps", name: "awesome", init: function () {
+      this.id = "ps";
+      this.name = "awesome";
+    }});
+}
 
 describe('객체 확인', function () {
   class TestObj {
@@ -13,11 +21,6 @@ describe('객체 확인', function () {
       return this._name;
     }
   }
-
-  beforeEach(() => {
-
-  });
-
 
   it('should be equals new class name and name', function () {
     assert.strictEqual(new TestObj({name: "ps"}).name, "ps");
@@ -36,43 +39,85 @@ describe('객체 확인', function () {
     assert.notStrictEqual(obj1, obj2);
   });
 
-  function Generate() {
-    return new Object({id: "ps", name: "awesome", init: () => {
-        this.id = "ps";
-        this.name = "awesome";
-      }});
-  }
+  let obj, obj_2;
+
+  beforeEach(() => {
+    obj = new Generate();
+    obj_2 = new Generate();
+  })
+
+  afterEach(() => {
+    obj = obj_2 = undefined;
+  })
 
   it('should be not equals properties in new Objects', () => {
-    const obj = new Generate();
+    obj = new Generate();
     assert.strictEqual(obj.id, "ps");
     assert.deepStrictEqual(obj.name, "awesome");
 
-    const obj2 = new Generate();
-    assert.strictEqual(obj2.id, "ps");
-    assert.deepStrictEqual(obj2.name, "awesome");
+    obj_2 = new Generate();
+    assert.strictEqual(obj_2.id, "ps");
+    assert.deepStrictEqual(obj_2.name, "awesome");
   });
 
-  it('should be ', function () {
-    const obj = new Generate();
+  it('should be obj have nothing to do with obj_2', function () {
+    obj = new Generate();
+    obj_2 = new Generate();
 
     obj.id = "psk";
+
     obj.name = "bird";
+    assert.deepStrictEqual(obj.id, "psk");
+
+    assert.deepStrictEqual(obj.name, "bird");
+
+    assert.strictEqual(obj_2.id, "ps");
+    assert.deepStrictEqual(obj_2.name, "awesome");
+
+    obj_2.id = "nullPoint";
+    obj_2.name = "exception";
+    assert.deepStrictEqual(obj_2.id, "nullPoint");
+    assert.deepStrictEqual(obj_2.name, "exception");
+    
+  });
+
+  it('should be obj.init() have nothing to do with obj_2', function () {
+    obj_2.id = "relu";
+    obj_2.name = "keras"
 
     // init 재정의
     obj.init = function() {this.id = "news";}
 
-    assert.deepStrictEqual(obj.id, "psk");
-    assert.deepStrictEqual(obj.name, "bird");
     obj.init();
-    assert.notStrictEqual(obj.id, "ps")
+    assert.notStrictEqual(obj.id, "psk")
     assert.strictEqual(obj.id, "news");
 
+    assert.deepStrictEqual(obj_2.id, "relu");
+    assert.deepStrictEqual(obj_2.name, "keras");
 
-    const obj2 = new Generate();
+  });
 
-    assert.strictEqual(obj2.id, "ps");
-    assert.deepStrictEqual(obj2.name, "awesome");
+  it('should be obj.init() override have nothing to do with obj_2.init()', function () {
+
+    obj_2.id = "tensorflow";
+    obj_2.name = "keras"
+
+    assert.deepStrictEqual(obj_2.id, "tensorflow");
+    assert.deepStrictEqual(obj_2.name, "keras");
+
+    // init 재정의
+    obj.init = function() {this.id = "news";}
+
+    obj.init();
+    assert.notStrictEqual(obj.id, "psk")
+    assert.strictEqual(obj.id, "news");
+
+    obj_2.init();
+    assert.deepStrictEqual(obj.id, "news");
+    assert.deepStrictEqual(obj.name, "awesome");
+
+    assert.deepStrictEqual(obj_2.id, "ps");
+    assert.deepStrictEqual(obj_2.name, "awesome");
 
   });
 });
