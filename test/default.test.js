6d942f78255670dbf9ad00bea2506aef650fa54c
PSK <wiv821253@gmail.com>
2020-07-28 10:53:34 +0900

    doc 문서 추가

--- test/default.test.js
+++ test/default.test.js
@@ -1,8 +1,20 @@
 module.exports = {
   assert: require("assert"),
+
+  // tag::mocha[]
   mocha: require("mocha"),
   describe: require("mocha").describe,
   it: require("mocha").it,
+  // end::mocha[]
+
+
+  // tag::chai[]
+  /*
+    how to use
+    https://www.chaijs.com/guide/styles/
+   */
+  chai: require("chai"),
   chaiAssert: require("chai").assert,
   chaiExpect: require("chai").expect
+  // end::chai[]
 };
--- test/editor/_000_init_use_chai.test.js
+++ test/editor/_000_init_use_chai.test.js
@@ -1,12 +1,17 @@
 const Timeline = require('./timeline');
 const assert = require('../default.test').chaiAssert;
 const expect = require('../default.test').chaiExpect;
+const should = require('../default.test').chai.should();
 
 describe('testInit', function () {
 
   it('should be not null', function () {
     const tl = Timeline.init();
     expect(tl.name).to.equal("PS")
+
     assert.equal(tl.name, "PS", 'tl name is `PS`')
+
+    tl.name.should.be.a("string")
+    tl.name.should.equal("PS")
   });
 });
