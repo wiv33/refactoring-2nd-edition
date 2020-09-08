0e352cbe42536862385b107545b9e03a019f69dc
PSK <wiv821253@gmail.com>
2020-09-08 13:05:23 +0900

    add parse

--- test/json_parse/json_parse.test.js
+++ test/json_parse/json_parse.test.js
@@ -1,11 +1,25 @@
 const common = require("../default.test");
 const text = require("./target_json");
+const chai = common.chai;
 const chaiAssert = common.chaiAssert;
 const chaiExpected = common.chaiExpect
 
 describe('JSON.parse testing', function () {
 
   it('should be occurred syntax error', function () {
-    chaiAssert.strictEqual(typeof text.my_text, "object")
+    // chai.expect(JSON.parse(text.my_string), "my string parsing error")
+    chaiAssert.strictEqual(typeof text.my_string, "string", "my text is string");
+    // console.info(JSON.stringify(text.my_string));
+    const regExpMatchArray = JSON.stringify(text.my_string).match(/(?:<iframe[^>]*)(?:(?:\/>)|(?:>.*?<\/iframe>))/gi);
+    chai.assert.strictEqual(regExpMatchArray.length, 3);
+    let resText = text.my_string
+    regExpMatchArray.forEach(s => {
+      // console.info(s.replace(/\\"|"/g, '__`'));
+      resText = resText.replace(s.replace(/\\"|"/g, '"'),
+        s.replace(/"/g, "`"))
+      console.info(resText)
+    });
+
+    console.info(JSON.parse(resText));
   });
 });
