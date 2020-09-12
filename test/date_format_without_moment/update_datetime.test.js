57a62260bf10c99d0280b5bdab887a73a896ebe5
PSK <40161102+wiv33@users.noreply.github.com>
2020-09-12 19:48:01 +0900

    init test

---
+++ test/date_format_without_moment/update_datetime.test.js
@@ -0,0 +1,11 @@
+const common = require("../default.test");
+const chai = common.chai;
+const chaiAssert = common.chaiAssert();
+const it = common.it;
+const datetime = require(`./datetime`);
+
+describe('datetime add', function () {
+  it('should be plus one hour without moment', function () {
+
+  });
+});
