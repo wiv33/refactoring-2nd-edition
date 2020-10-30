d1a79246020b36fb04169fa836498d4067bb6726
PSK <wiv821253@gmail.com>
2020-10-30 11:03:50 +0900

    extract key, youtube 포멧이 바뀔 가능성이 낮다고 가정

--- test/youtube_get_thumb/app.test_01.js
+++ test/youtube_get_thumb/app.test_01.js
@@ -7,36 +7,45 @@ const before = mocha.before;
 const extractKey = require("./extractKey");
 
 describe('# 유튜브 uri 조합', () => {
-    let thumbBase = "https://img.youtube.com/vi/{{key}}/hqdefault.jpg"
+  let thumbBase = "https://img.youtube.com/vi/{{key}}/hqdefault.jpg"
 
-    let uri
-    let watch
+  let uri
+  let watch
+  let youtuBe
 
-    let expected = `https://img.youtube.com/vi/k-senVR0q8M/hqdefault.jpg`
-    let actual
+  let expected = `https://img.youtube.com/vi/k-senVR0q8M/hqdefault.jpg`
+  let actual
 
-    let expectedKey
-    let actualKey
+  let expectedKey
+  let actualKey
 
-    before(() => {
-        uri = `https://www.youtube.com/embed/k-senVR0q8M?list=PL7FKPrjfmaNx4O5PzFuXm3RTAAgEyKjds?wmode=transparent`;
-        watch = `https://www.youtube.com/watch?v=G4tvbcevOH8&t=9s`
-    })
+  before(() => {
+    uri = `https://www.youtube.com/embed/k-senVR0q8M?list=PL7FKPrjfmaNx4O5PzFuXm3RTAAgEyKjds?wmode=transparent`;
+    watch = `https://www.youtube.com/watch?v=G4tvbcevOH8&t=9s`
+    youtuBe = `https://youtu.be/G4tvbcevOH8`
+  })
 
-    it('parameter 있을 때', () => {
-        expectedKey = `k-senVR0q8M`
-        actualKey = extractKey(uri);
-        assert.strictEqual(actualKey, expectedKey, "actual key equal extract key")
-    })
+  it('parameter 있을 때', () => {
+    expectedKey = `k-senVR0q8M`
+    actualKey = extractKey(uri);
+    assert.strictEqual(actualKey, expectedKey, "actual key equal extract key")
+  })
 
-    it('uri 전체 조합 가져오기', () => {
-        actual = thumbBase.replace("{{key}}", extractKey(uri));
-        assert.strictEqual(actual, expected, "uri equal")
-    })
+  it('uri 전체 조합 가져오기', () => {
+    actual = thumbBase.replace("{{key}}", extractKey(uri));
+    assert.strictEqual(actual, expected, "uri equal")
+  })
 
-    it('should be extracted uri in watch', function () {
-        actualKey = extractKey(watch);
-        assert.strictEqual(actualKey, expected);
-    });
+  it('should be extracted uri in watch', function () {
+    actualKey = extractKey(watch);
+    expectedKey = `G4tvbcevOH8`
+    assert.strictEqual(actualKey, expectedKey);
+  });
+
+  it('should be extract youtu.......', function () {
+    actualKey = extractKey(youtuBe)
+    expectedKey = `G4tvbcevOH8`
+    assert.strictEqual(actualKey, expectedKey);
+  });
 })
 
