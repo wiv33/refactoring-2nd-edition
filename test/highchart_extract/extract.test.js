3b356f7f7d98b48d6b6d948a782fd6af2584d4fe
PSK <wiv821253@gmail.com>
2021-01-13 18:36:50 +0900

    obj 안에 존재하는 name 변경하기

--- test/highchart_extract/extract.test.js
+++ test/highchart_extract/extract.test.js
@@ -4,6 +4,7 @@ const it = common.it;
 const chaiExpect = common.chaiExpect;
 const _ = require('lodash');
 
+const korData = require('./korData.js');
 const data = require('./jsonData.js');
 
 function extractCodeName(feature) {
@@ -17,5 +18,19 @@ describe('extract test', function () {
     it('should be extract iso-a3 and name in features', function () {
         const result = _.map(data.features, extractCodeName);
         chaiExpect(result.length).eq(data.features.length)
+    });
+
+    it('should be merging kor name and json data', function () {
+        const korCode = korData.map(m => m.code);
+        data.features = _.map(data.features, f => {
+            const find = _.find(f.properties, o => korCode.includes(o));
+            if (!find) {
+                return
+            }
+            f.properties.name = korData[korCode.indexOf(find)].nameKor
+            return f
+        })
+
+        console.log(JSON.stringify(data));
     });
 });
---
+++ test/highchart_extract/korData.js
@@ -0,0 +1,541 @@
+module.exports = [{"code": "GHA", "name": "Ghana", "nameKor": "가나", "value": 0}, {
+    "code": "GAB",
+    "name": "Gabon",
+    "nameKor": "가봉",
+    "value": 0
+}, {"code": "GUY", "name": "Guyana", "nameKor": "가이아나", "value": 0}, {
+    "code": "GMB",
+    "name": "Gambia,The",
+    "nameKor": "감비아",
+    "value": 0
+}, {"code": "GTM", "name": "Guatemala", "nameKor": "과테말라", "value": 0}, {
+    "code": "GUM",
+    "name": "Guam",
+    "nameKor": "괌",
+    "value": 0
+}, {"code": "GRD", "name": "Grenada", "nameKor": "그레나다", "value": 0}, {
+    "code": "GRC",
+    "name": "Greece",
+    "nameKor": "그리스",
+    "value": 0
+}, {"code": "GRL", "name": "Greenland", "nameKor": "그린란드", "value": 0}, {
+    "code": "GIN",
+    "name": "Guinea",
+    "nameKor": "기니",
+    "value": 0
+}, {"code": "GNB", "name": "Guinea-Bissau", "nameKor": "기니비사우", "value": 0}, {
+    "code": "NAM",
+    "name": "Namibia",
+    "nameKor": "나미비아",
+    "value": 0
+}, {"code": "NRU", "name": "Nauru", "nameKor": "나우루", "value": 0}, {
+    "code": "NGA",
+    "name": "Nigeria",
+    "nameKor": "나이지리아",
+    "value": 0
+}, {"code": "ZAF", "name": "SouthAfrica", "nameKor": "남아프리카공화국", "value": 0}, {
+    "code": "NLD",
+    "name": "Netherlands",
+    "nameKor": "네덜란드",
+    "value": 0
+}, {"code": "NPL", "name": "Nepal", "nameKor": "네팔", "value": 0}, {
+    "code": "NOR",
+    "name": "Norway",
+    "nameKor": "노르웨이",
+    "value": 0
+}, {"code": "NCL", "name": "NewCaledonia", "nameKor": "누벨칼레도니", "value": 0}, {
+    "code": "NZL",
+    "name": "NewZealand",
+    "nameKor": "뉴질랜드",
+    "value": 0
+}, {"code": "NER", "name": "Niger", "nameKor": "니제르", "value": 0}, {
+    "code": "NIC",
+    "name": "Nicaragua",
+    "nameKor": "니카라과",
+    "value": 0
+}, {"code": "KOR", "name": "Korea,Rep.", "nameKor": "대한민국", "value": 0}, {
+    "code": "TWN",
+    "name": "Taiwan",
+    "nameKor": "대만",
+    "value": 0
+}, {"code": "DNK", "name": "Denmark", "nameKor": "덴마크", "value": 0}, {
+    "code": "DOM",
+    "name": "DominicanRepublic",
+    "nameKor": "도미니카공화국",
+    "value": 0
+}, {"code": "DMA", "name": "Dominica", "nameKor": "도미니카연방", "value": 0}, {
+    "code": "DEU",
+    "name": "Germany",
+    "nameKor": "독일",
+    "value": 0
+}, {"code": "TLS", "name": "Timor-Leste", "nameKor": "동티모르", "value": 0}, {
+    "code": "LAO",
+    "name": "LaoPDR",
+    "nameKor": "라오스",
+    "value": 0
+}, {"code": "LBR", "name": "Liberia", "nameKor": "라이베리아", "value": 0}, {
+    "code": "LVA",
+    "name": "Latvia",
+    "nameKor": "라트비아",
+    "value": 0
+}, {"code": "RUS", "name": "RussianFederation", "nameKor": "러시아", "value": 0}, {
+    "code": "LBN",
+    "name": "Lebanon",
+    "nameKor": "레바논",
+    "value": 0
+}, {"code": "LSO", "name": "Lesotho", "nameKor": "레소토", "value": 0}, {
+    "code": "ROU",
+    "name": "Romania",
+    "nameKor": "루마니아",
+    "value": 0
+}, {"code": "LUX", "name": "Luxembourg", "nameKor": "룩셈부르크", "value": 0}, {
+    "code": "RWA",
+    "name": "Rwanda",
+    "nameKor": "르완다",
+    "value": 0
+}, {"code": "LBY", "name": "Libya", "nameKor": "리비아", "value": 0}, {
+    "code": "LTU",
+    "name": "Lithuania",
+    "nameKor": "리투아니아",
+    "value": 0
+}, {"code": "LIE", "name": "Liechtenstein", "nameKor": "리히텐슈타인", "value": 0}, {
+    "code": "MDG",
+    "name": "Madagascar",
+    "nameKor": "마다가스카르",
+    "value": 0
+}, {"code": "MHL", "name": "MarshallIslands", "nameKor": "마셜제도", "value": 0}, {
+    "code": "MAC",
+    "name": "MacaoSAR,China",
+    "nameKor": "마카오",
+    "value": 0
+}, {"code": "MWI", "name": "Malawi", "nameKor": "말라위", "value": 0}, {
+    "code": "MYS",
+    "name": "Malaysia",
+    "nameKor": "말레이시아",
+    "value": 0
+}, {"code": "MLI", "name": "Mali", "nameKor": "말리", "value": 0}, {
+    "code": "IMN",
+    "name": "IsleofMan",
+    "nameKor": "맨섬",
+    "value": 0
+}, {"code": "MEX", "name": "Mexico", "nameKor": "멕시코", "value": 0}, {
+    "code": "MCO",
+    "name": "Monaco",
+    "nameKor": "모나코",
+    "value": 0
+}, {"code": "MAR", "name": "Morocco", "nameKor": "모로코", "value": 0}, {
+    "code": "MUS",
+    "name": "Mauritius",
+    "nameKor": "모리셔스",
+    "value": 0
+}, {"code": "MRT", "name": "Mauritania", "nameKor": "모리타니", "value": 0}, {
+    "code": "MOZ",
+    "name": "Mozambique",
+    "nameKor": "모잠비크",
+    "value": 0
+}, {"code": "MNE", "name": "Montenegro", "nameKor": "몬테네그로", "value": 0}, {
+    "code": "MDA",
+    "name": "Moldova",
+    "nameKor": "몰도바",
+    "value": 0
+}, {"code": "MDV", "name": "Maldives", "nameKor": "몰디브", "value": 0}, {
+    "code": "MLT",
+    "name": "Malta",
+    "nameKor": "몰타",
+    "value": 0
+}, {"code": "MNG", "name": "Mongolia", "nameKor": "몽골", "value": 0}, {
+    "code": "USA",
+    "name": "UnitedStates",
+    "nameKor": "미국",
+    "value": 0
+}, {"code": "VIR", "name": "VirginIslands(U.S.)", "nameKor": "미국령버진아일랜드", "value": 0}, {
+    "code": "MMR",
+    "name": "Myanmar",
+    "nameKor": "미얀마",
+    "value": 0
+}, {"code": "FSM", "name": "Micronesia,Fed.Sts.", "nameKor": "미크로네시아연방", "value": 0}, {
+    "code": "VUT",
+    "name": "Vanuatu",
+    "nameKor": "바누아투",
+    "value": 0
+}, {"code": "BHR", "name": "Bahrain", "nameKor": "바레인", "value": 0}, {
+    "code": "BRB",
+    "name": "Barbados",
+    "nameKor": "바베이도스",
+    "value": 0
+}, {"code": "BHS", "name": "Bahamas,The", "nameKor": "바하마", "value": 0}, {
+    "code": "BGD",
+    "name": "Bangladesh",
+    "nameKor": "방글라데시",
+    "value": 0
+}, {"code": "BMU", "name": "Bermuda", "nameKor": "버뮤다", "value": 0}, {
+    "code": "BEN",
+    "name": "Benin",
+    "nameKor": "베냉",
+    "value": 0
+}, {"code": "VEN", "name": "Venezuela,RB", "nameKor": "베네수엘라", "value": 0}, {
+    "code": "VNM",
+    "name": "Vietnam",
+    "nameKor": "베트남",
+    "value": 0
+}, {"code": "BEL", "name": "Belgium", "nameKor": "벨기에", "value": 0}, {
+    "code": "BLR",
+    "name": "Belarus",
+    "nameKor": "벨로루시",
+    "value": 0
+}, {"code": "BLZ", "name": "Belize", "nameKor": "벨리즈", "value": 0}, {
+    "code": "BIH",
+    "name": "BosniaandHerzegovina",
+    "nameKor": "보스니아헤르체고비나",
+    "value": 0
+}, {"code": "BWA", "name": "Botswana", "nameKor": "보츠와나", "value": 0}, {
+    "code": "BOL",
+    "name": "Bolivia",
+    "nameKor": "볼리비아",
+    "value": 0
+}, {"code": "BDI", "name": "Burundi", "nameKor": "부룬디", "value": 0}, {
+    "code": "BFA",
+    "name": "BurkinaFaso",
+    "nameKor": "부르키나파소",
+    "value": 0
+}, {"code": "BTN", "name": "Bhutan", "nameKor": "부탄", "value": 0}, {
+    "code": "MNP",
+    "name": "NorthernMarianaIslands",
+    "nameKor": "북마리아나제도",
+    "value": 0
+}, {"code": "MKD", "name": "Macedonia,FYR", "nameKor": "북마케도니아", "value": 0}, {
+    "code": "NAC",
+    "name": "NorthAmerica",
+    "nameKor": "북아메리카",
+    "value": 0
+}, {"code": "PRK", "name": "Korea", "nameKor": "북한", "value": 0}, {
+    "code": "BGR",
+    "name": "Bulgaria",
+    "nameKor": "불가리아",
+    "value": 0
+}, {"code": "BRA", "name": "Brazil", "nameKor": "브라질", "value": 0}, {
+    "code": "BRN",
+    "name": "BruneiDarussalam",
+    "nameKor": "브루나이",
+    "value": 0
+}, {"code": "WSM", "name": "Samoa", "nameKor": "사모아", "value": 0}, {
+    "code": "SAU",
+    "name": "SaudiArabia",
+    "nameKor": "사우디아라비아",
+    "value": 0
+}, {"code": "SMR", "name": "SanMarino", "nameKor": "산마리노", "value": 0}, {
+    "code": "STP",
+    "name": "SaoTomeandPrincipe",
+    "nameKor": "상투메프린시페",
+    "value": 0
+}, {"code": "MAF", "name": "St.Martin(Frenchpart)", "nameKor": "생마르탱", "value": 0}, {
+    "code": "SEN",
+    "name": "Senegal",
+    "nameKor": "세네갈",
+    "value": 0
+}, {"code": "SRB", "name": "Serbia", "nameKor": "세르비아", "value": 0}, {
+    "code": "SYC",
+    "name": "Seychelles",
+    "nameKor": "세이셸",
+    "value": 0
+}, {"code": "LCA", "name": "St.Lucia", "nameKor": "세인트루시아", "value": 0}, {
+    "code": "VCT",
+    "name": "St.VincentandtheGrenadines",
+    "nameKor": "세인트빈센트그레나딘",
+    "value": 0
+}, {"code": "KNA", "name": "St.KittsandNevis", "nameKor": "세인트키츠네비스", "value": 0}, {
+    "code": "SOM",
+    "name": "Somalia",
+    "nameKor": "소말리아",
+    "value": 0
+}, {"code": "SLB", "name": "SolomonIslands", "nameKor": "솔로몬제도", "value": 0}, {
+    "code": "SDN",
+    "name": "Sudan",
+    "nameKor": "수단",
+    "value": 0
+}, {"code": "SUR", "name": "Suriname", "nameKor": "수리남", "value": 0}, {
+    "code": "LKA",
+    "name": "SriLanka",
+    "nameKor": "스리랑카",
+    "value": 0
+}, {"code": "SWE", "name": "Sweden", "nameKor": "스웨덴", "value": 0}, {
+    "code": "CHE",
+    "name": "Switzerland",
+    "nameKor": "스위스",
+    "value": 0
+}, {"code": "ESP", "name": "Spain", "nameKor": "스페인", "value": 0}, {
+    "code": "SVK",
+    "name": "SlovakRepublic",
+    "nameKor": "슬로바키아",
+    "value": 0
+}, {"code": "SVN", "name": "Slovenia", "nameKor": "슬로베니아", "value": 0}, {
+    "code": "SYR",
+    "name": "SyrianArabRepublic",
+    "nameKor": "시리아",
+    "value": 0
+}, {"code": "SLE", "name": "SierraLeone", "nameKor": "시에라리온", "value": 0}, {
+    "code": "SXM",
+    "name": "SintMaarten(Dutchpart)",
+    "nameKor": "신트마르턴",
+    "value": 0
+}, {"code": "SGP", "name": "Singapore", "nameKor": "싱가포르", "value": 0}, {
+    "code": "ARE",
+    "name": "UnitedArabEmirates",
+    "nameKor": "아랍에미리트",
+    "value": 0
+}, {"code": "ABW", "name": "Aruba", "nameKor": "아루바", "value": 0}, {
+    "code": "ARM",
+    "name": "Armenia",
+    "nameKor": "아르메니아",
+    "value": 0
+}, {"code": "ARG", "name": "Argentina", "nameKor": "아르헨티나", "value": 0}, {
+    "code": "ASM",
+    "name": "AmericanSamoa",
+    "nameKor": "아메리칸사모아",
+    "value": 0
+}, {"code": "ISL", "name": "Iceland", "nameKor": "아이슬란드", "value": 0}, {
+    "code": "HTI",
+    "name": "Haiti",
+    "nameKor": "아이티",
+    "value": 0
+}, {"code": "IRL", "name": "Ireland", "nameKor": "아일랜드", "value": 0}, {
+    "code": "AZE",
+    "name": "Azerbaijan",
+    "nameKor": "아제르바이잔",
+    "value": 0
+}, {"code": "AFG", "name": "Afghanistan", "nameKor": "아프가니스탄", "value": 0}, {
+    "code": "AND",
+    "name": "Andorra",
+    "nameKor": "안도라",
+    "value": 0
+}, {"code": "ALB", "name": "Albania", "nameKor": "알바니아", "value": 0}, {
+    "code": "DZA",
+    "name": "Algeria",
+    "nameKor": "알제리",
+    "value": 0
+}, {"code": "AGO", "name": "Angola", "nameKor": "앙골라", "value": 0}, {
+    "code": "ATG",
+    "name": "AntiguaandBarbuda",
+    "nameKor": "앤티가바부다",
+    "value": 0
+}, {"code": "SWZ", "name": "Swaziland", "nameKor": "에스와티니", "value": 0}, {
+    "code": "EST",
+    "name": "Estonia",
+    "nameKor": "에스토니아",
+    "value": 0
+}, {"code": "ECU", "name": "Ecuador", "nameKor": "에콰도르", "value": 0}, {
+    "code": "ETH",
+    "name": "Ethiopia",
+    "nameKor": "에티오피아",
+    "value": 0
+}, {"code": "SLV", "name": "ElSalvador", "nameKor": "엘살바도르", "value": 0}, {
+    "code": "GBR",
+    "name": "UnitedKingdom",
+    "nameKor": "영국",
+    "value": 0
+}, {"code": "VGB", "name": "BritishVirginIslands", "nameKor": "영국령버진아일랜드", "value": 0}, {
+    "code": "YEM",
+    "name": "Yemen,Rep.",
+    "nameKor": "예맨",
+    "value": 0
+}, {"code": "OMN", "name": "Oman", "nameKor": "오만", "value": 0}, {
+    "code": "AUT",
+    "name": "Austria",
+    "nameKor": "오스트리아",
+    "value": 0
+}, {"code": "HND", "name": "Honduras", "nameKor": "온두라스", "value": 0}, {
+    "code": "JOR",
+    "name": "Jordan",
+    "nameKor": "요르단",
+    "value": 0
+}, {"code": "UGA", "name": "Uganda", "nameKor": "우간다", "value": 0}, {
+    "code": "URY",
+    "name": "Uruguay",
+    "nameKor": "우루과이",
+    "value": 0
+}, {"code": "UZB", "name": "Uzbekistan", "nameKor": "우즈베키스탄", "value": 0}, {
+    "code": "UKR",
+    "name": "Ukraine",
+    "nameKor": "우크라이나",
+    "value": 0
+}, {"code": "PSE", "name": "WestBankandGaza", "nameKor": "웨스트뱅크", "value": 0}, {
+    "code": "IRQ",
+    "name": "Iraq",
+    "nameKor": "이라크",
+    "value": 0
+}, {"code": "IRN", "name": "Iran,IslamicRep.", "nameKor": "이란", "value": 0}, {
+    "code": "ISR",
+    "name": "Israel",
+    "nameKor": "이스라엘",
+    "value": 0
+}, {"code": "EGY", "name": "Egypt,ArabRep.", "nameKor": "이집트", "value": 0}, {
+    "code": "ITA",
+    "name": "Italy",
+    "nameKor": "이탈리아",
+    "value": 0
+}, {"code": "IND", "name": "India", "nameKor": "인도", "value": 0}, {
+    "code": "IDN",
+    "name": "Indonesia",
+    "nameKor": "인도네시아",
+    "value": 0
+}, {"code": "JPN", "name": "Japan", "nameKor": "일본", "value": 0}, {
+    "code": "JAM",
+    "name": "Jamaica",
+    "nameKor": "자메이카",
+    "value": 0
+}, {"code": "ZMB", "name": "Zambia", "nameKor": "잠비아", "value": 0}, {
+    "code": "GNQ",
+    "name": "EquatorialGuinea",
+    "nameKor": "적도기니공화국",
+    "value": 0
+}, {"code": "GEO", "name": "Georgia", "nameKor": "조지아", "value": 0}, {
+    "code": "CHN",
+    "name": "China",
+    "nameKor": "중국",
+    "value": 0
+}, {"code": "CAF", "name": "CentralAfricanRepublic", "nameKor": "중앙아프리카공화국", "value": 0}, {
+    "code": "DJI",
+    "name": "Djibouti",
+    "nameKor": "지부티",
+    "value": 0
+}, {"code": "GIB", "name": "Gibraltar", "nameKor": "지브롤터", "value": 0}, {
+    "code": "ZWE",
+    "name": "Zimbabwe",
+    "nameKor": "짐바브웨",
+    "value": 0
+}, {"code": "TCD", "name": "Chad", "nameKor": "차드", "value": 0}, {
+    "code": "CHI",
+    "name": "ChannelIslands",
+    "nameKor": "채널제도",
+    "value": 0
+}, {"code": "CZE", "name": "CzechRepublic", "nameKor": "체코", "value": 0}, {
+    "code": "CHL",
+    "name": "Chile",
+    "nameKor": "칠레",
+    "value": 0
+}, {"code": "CMR", "name": "Cameroon", "nameKor": "카메룬", "value": 0}, {
+    "code": "CPV",
+    "name": "CaboVerde",
+    "nameKor": "카보베르데",
+    "value": 0
+}, {"code": "KAZ", "name": "Kazakhstan", "nameKor": "카자흐스탄", "value": 0}, {
+    "code": "QAT",
+    "name": "Qatar",
+    "nameKor": "카타르",
+    "value": 0
+}, {"code": "KHM", "name": "Cambodia", "nameKor": "캄보디아", "value": 0}, {
+    "code": "CAN",
+    "name": "Canada",
+    "nameKor": "캐나다",
+    "value": 0
+}, {"code": "KEN", "name": "Kenya", "nameKor": "케냐", "value": 0}, {
+    "code": "CYM",
+    "name": "CaymanIslands",
+    "nameKor": "케이맨제도",
+    "value": 0
+}, {"code": "COM", "name": "Comoros", "nameKor": "코모로", "value": 0}, {
+    "code": "XKX",
+    "name": "Kosovo",
+    "nameKor": "코소보",
+    "value": 0
+}, {"code": "CRI", "name": "CostaRica", "nameKor": "코스타리카", "value": 0}, {
+    "code": "CIV",
+    "name": "Coted'Ivoire",
+    "nameKor": "코트디부아르",
+    "value": 0
+}, {"code": "COL", "name": "Colombia", "nameKor": "콜롬비아", "value": 0}, {
+    "code": "COG",
+    "name": "Congo,Rep.",
+    "nameKor": "콩고공화국",
+    "value": 0
+}, {"code": "COD", "name": "Congo,Dem.Rep.", "nameKor": "콩고민주공화국", "value": 0}, {
+    "code": "CUB",
+    "name": "Cuba",
+    "nameKor": "쿠바",
+    "value": 0
+}, {"code": "KWT", "name": "Kuwait", "nameKor": "쿠웨이트", "value": 0}, {
+    "code": "CUW",
+    "name": "Curacao",
+    "nameKor": "퀴라소",
+    "value": 0
+}, {"code": "HRV", "name": "Croatia", "nameKor": "크로아티아", "value": 0}, {
+    "code": "KGZ",
+    "name": "KyrgyzRepublic",
+    "nameKor": "키르기스스탄",
+    "value": 0
+}, {"code": "KIR", "name": "Kiribati", "nameKor": "키리바시", "value": 0}, {
+    "code": "CYP",
+    "name": "Cyprus",
+    "nameKor": "키프로스",
+    "value": 0
+}, {"code": "TJK", "name": "Tajikistan", "nameKor": "타지키스탄", "value": 0}, {
+    "code": "TZA",
+    "name": "Tanzania",
+    "nameKor": "탄자니아",
+    "value": 0
+}, {"code": "THA", "name": "Thailand", "nameKor": "태국", "value": 0}, {
+    "code": "TCA",
+    "name": "TurksandCaicosIslands",
+    "nameKor": "터크스케이커스제도",
+    "value": 0
+}, {"code": "TUR", "name": "Turkey", "nameKor": "터키", "value": 0}, {
+    "code": "TGO",
+    "name": "Togo",
+    "nameKor": "토고",
+    "value": 0
+}, {"code": "TON", "name": "Tonga", "nameKor": "통가", "value": 0}, {
+    "code": "TKM",
+    "name": "Turkmenistan",
+    "nameKor": "투르크메니스탄",
+    "value": 0
+}, {"code": "TUV", "name": "Tuvalu", "nameKor": "투발루", "value": 0}, {
+    "code": "TUN",
+    "name": "Tunisia",
+    "nameKor": "튀니지",
+    "value": 0
+}, {"code": "TTO", "name": "TrinidadandTobago", "nameKor": "트리니다드토바고", "value": 0}, {
+    "code": "PAN",
+    "name": "Panama",
+    "nameKor": "파나마",
+    "value": 0
+}, {"code": "PRY", "name": "Paraguay", "nameKor": "파라과이", "value": 0}, {
+    "code": "PAK",
+    "name": "Pakistan",
+    "nameKor": "파키스탄",
+    "value": 0
+}, {"code": "PNG", "name": "PapuaNewGuinea", "nameKor": "파푸아뉴기니", "value": 0}, {
+    "code": "PLW",
+    "name": "Palau",
+    "nameKor": "팔라우",
+    "value": 0
+}, {"code": "FRO", "name": "FaroeIslands", "nameKor": "페로제도", "value": 0}, {
+    "code": "PER",
+    "name": "Peru",
+    "nameKor": "페루",
+    "value": 0
+}, {"code": "PRT", "name": "Portugal", "nameKor": "포르투갈", "value": 0}, {
+    "code": "POL",
+    "name": "Poland",
+    "nameKor": "폴란드",
+    "value": 0
+}, {"code": "PRI", "name": "PuertoRico", "nameKor": "푸에르토리코", "value": 0}, {
+    "code": "FRA",
+    "name": "France",
+    "nameKor": "프랑스",
+    "value": 0
+}, {"code": "PYF", "name": "FrenchPolynesia", "nameKor": "프랑스령폴리네시아", "value": 0}, {
+    "code": "FJI",
+    "name": "Fiji",
+    "nameKor": "피지",
+    "value": 0
+}, {"code": "FIN", "name": "Finland", "nameKor": "핀란드", "value": 0}, {
+    "code": "PHL",
+    "name": "Philippines",
+    "nameKor": "필리핀",
+    "value": 0
+}, {"code": "HUN", "name": "Hungary", "nameKor": "헝가리", "value": 0}, {
+    "code": "AUS",
+    "name": "Australia",
+    "nameKor": "호주",
+    "value": 0
+}, {"code": "HKG", "name": "HongKongSAR,China", "nameKor": "홍콩", "value": 0}]
